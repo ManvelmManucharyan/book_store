9aa276d665ef3dfa627a5858c1e3aaa0ed95b0b5
ManvelmManucharyan <manvelmanucharyan98@gmail.com>
2022-06-22 00:13:05 +0400

    try catch

--- controllers/BookControllers.js
+++ controllers/BookControllers.js
@@ -2,44 +2,76 @@ const UserServices = require('../service/BookServices')
 
 class BookControllers {
     static async getAllBooks(req,res){
-        const books = await UserServices.getAllBooks(req.query)
-        res.status(200).send(books)
+        try {
+            const books = await UserServices.getAllBooks(req.query)
+            res.status(200).send(books)
+        }catch (err) {
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async getAllBooksById(req,res){
-        const book = await UserServices.getAllBooksById(req.params.id)
-        res.status(200).send(book)
+        try{
+            const book = await UserServices.getAllBooksById(req.params.id)
+            res.status(200).send(book)
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async getAllBooksByBookTitle(req,res){
-        const book = await UserServices.getAllBooksByBookTitle(req.params.title)
-        res.status(200).send(book)
+        try{
+            const book = await UserServices.getAllBooksByBookTitle(req.params.title)
+            res.status(200).send(book)
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async getAllBooksByCategory(req,res){
-        const book = await UserServices.getAllBooksByCategory(req.params.category)
-        res.status(200).send(book)
+        try{
+            const book = await UserServices.getAllBooksByCategory(req.params.category)
+            res.status(200).send(book)
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async getAllBooksByPrice(req,res){
-        const book = await UserServices.getAllBooksByPrice(req.params.price)
-        res.status(200).send(book)
+        try{
+            const book = await UserServices.getAllBooksByPrice(req.params.price)
+            res.status(200).send(book)
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async createBook(req, res){
-        const {title,author,price,category} = req.body;
-        await UserServices.createBook(title,author,price,category);
-        res.status(201).send('Book successfully created');
+        try{
+            const {title,author,price,category} = req.body;
+            await UserServices.createBook(title,author,price,category);
+            res.status(201).send('Book successfully created');
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async deleteBook(req,res){
-        await UserServices.deleteBook(req.params.id);
-        res.status(201).send('Book successfully deleted')
+        try{
+            await UserServices.deleteBook(req.params.id);
+            res.status(201).send('Book successfully deleted')
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
     static async updateInfo(req,res){
-        await UserServices.updateInfo(req.body ,req.params.id)
-        res.status(201).send('Book info successfully updated')
+        try{
+            await UserServices.updateInfo(req.body ,req.params.id)
+            res.status(201).send('Book info successfully updated')
+        }catch (err){
+            res.status(404).send("Server didn't found");
+        }
     }
 
 }
